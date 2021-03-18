4494c473bdfb8aacd6d573e7fbc6d0959fabb068
mahdi <gharehnaz71@gmail.com>
2021-03-18 12:05:30 +0330

    template

---
+++ News.Web/News.Web/Areas/AdminHome/Controllers/AdminHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace News.Web.Areas.AdminHome.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        // GET: AdminHome/AdminHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
